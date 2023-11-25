4e7258c3dba12ac0a428e640297c22eae117f07b
melvyn212 <dehundeabdala@gmail.com>
2023-11-25 15:49:12 +0000

    Possibilité de mettre plusieur docker user

--- SHM/TEST tensorflow basique/ansible/set_up_jnano.sh
+++ SHM/TEST tensorflow basique/ansible/set_up_jnano.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y ansible
+ansible --version
 PLAYBOOK_PATH="set_up_jnano_executable.yml"
-ansible-playbook  $PLAYBOOK_PATH --ask-become-pass
+ansible-playbook $PLAYBOOK_PATH -i "localhost," --connection=local
 
 #taper ./set_up_jnano.sh
