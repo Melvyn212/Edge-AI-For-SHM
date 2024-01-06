8338e06a1349a530a92103b261dec123c3bdd5d5
Melvyn212 <114694633+Melvyn212@users.noreply.github.com>
2024-01-06 04:16:47 +0100

    Update DockerRun.sh

--- DockerRun/DockerRun.sh
+++ DockerRun/DockerRun.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 USER="adehundeag"
+
 # Définissez les chemins dans des variables
 TEGRASTATS_PATH="/usr/bin/tegrastats"
-CODE_PATH="/home/${USER}/Edge-AI-For-SHM/Core"
+CODE_PATH="/home/${USER}/Edge-AI-For-SHM/Core/MODEL/OmniAnomaly"
 OUTPUT_PATH="/home/${USER}/Edge-AI-For-SHM/output"
 DOCKER_IMAGE="melvyn212/edge_ai_jnano:latest"
 
-# Exécutez le conteneur Docker avec les volumes montés
-sudo docker run -it --rm \
+# Chemin vers les bibliothèques CUDA sur l'hôte
+CUDA_LIB_PATH="/usr/local/cuda/lib64"
+
+# Exécutez le conteneur Docker avec les volumes montés et le support CUDA
+sudo docker run -it --rm --runtime nvidia \
 -v "${TEGRASTATS_PATH}:${TEGRASTATS_PATH}:ro" \
 -v "${CODE_PATH}:/EdgeAI:ro" \
 -v "${OUTPUT_PATH}:/output" \
+-v "${CUDA_LIB_PATH}:${CUDA_LIB_PATH}:ro" \
+-e LD_LIBRARY_PATH="${CUDA_LIB_PATH}:$LD_LIBRARY_PATH" \
 "${DOCKER_IMAGE}"
-
-#./DockerRun.sh
